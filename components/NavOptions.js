e5587d8ae2d3595a819ea534188bc451de2af49a
Naman M Samarth <namansamarth1@gmail.com>
2022-02-01 17:48:53 +0530

    Mapped origin on map

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -4,6 +4,8 @@ import { TouchableOpacity } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import { Icon } from "react-native-elements/dist/icons/Icon";
 import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { selectOrigin } from "../slices/navSlice";
 
 const data = [
   {
@@ -22,6 +24,7 @@ const data = [
 
 const NavOptions = () => {
   const navigation = useNavigation();
+  const origin = useSelector(selectOrigin);
 
   return (
     <FlatList
@@ -32,8 +35,9 @@ const NavOptions = () => {
         <TouchableOpacity
           style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
           onPress={() => navigation.navigate(item.screen)}
+          disabled={!origin}
         >
-          <View>
+          <View style={tw`${!origin && "opacity-20"}`}>
             <Image
               style={{ width: 120, height: 120, resizeMode: "contain" }}
               source={{ uri: item.image }}
