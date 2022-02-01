7bb1ea372e5660c837e1fde58c0e952b9c197db8
Naman M Samarth <namansamarth1@gmail.com>
2022-02-01 14:00:32 +0530

    set up navigation init

---
+++ screens/MapScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text>Here is the map stuff ...</Text>
+    </View>
+  );
+};
+
+export default MapScreen;
+
