2d6ce13fa9176d9bdcf16d6536ad4794ee463fe9
Naman M Samarth <namansamarth1@gmail.com>
2022-02-01 22:36:56 +0530

    Added favourites

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,6 +6,7 @@ import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplet
 import { GOOGLE_MAPS_APIKEY } from "@env";
 import { useDispatch } from "react-redux";
 import { setOrigin, setDestination } from "../slices/navSlice";
+import NavFavourites from "../components/NavFavourites";
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
@@ -51,6 +52,7 @@ const HomeScreen = () => {
           debounce={400}
         />
         <NavOptions />
+        <NavFavourites />
       </View>
       <StatusBar />
     </SafeAreaView>
